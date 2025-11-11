5f15a5aee65d211c49d797ba7e52082d755d54e9
ronnystar564 <163534333+ronnystar564@users.noreply.github.com>
2025-11-11 14:33:12 +0530

    ca

---
+++ QMS/Controllers/CustomerCA.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace QMS.Controllers
+{
+    public class CustomerCA : Controller
+    {
+        public IActionResult CustomerRCA()
+        {
+            return View();
+        }
+        public IActionResult CustomerRCADetails()
+        {
+            return View();
+        }
+    }
+}

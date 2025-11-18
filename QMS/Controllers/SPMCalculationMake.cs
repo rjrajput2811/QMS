cc61eb73b4de4fe186a3574ff8b7da3c70890810
ronnystar564 <163534333+ronnystar564@users.noreply.github.com>
2025-11-18 11:39:52 +0530

    SMP caculation

---
+++ QMS/Controllers/SPMCalculationMake.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace QMS.Controllers
+{
+    public class SPMCalculationMake : Controller
+    {
+        public IActionResult SPMCalucationMakeDetails()
+        {
+            return View();
+        }
+        public IActionResult SPMCalucationMake()
+        {
+            return View();
+        }
+    }
+}

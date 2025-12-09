1e4996e3a77d8b99dce2e4f00856f5620b5c4e04
rjrajput2811 <Bigboss2811@>
2025-12-09 13:18:04 +0530

    RONAL Changes

---
+++ QMS.Core/Repositories/SixSigmaIndicesRepository/ISixSigmaIndicesRepository.cs
@@ -0,0 +1,15 @@
+﻿using QMS.Core.Models;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace QMS.Core.Repositories.SixSigmaIndicesRepo
+{
+    public interface ISixSigmaIndicesRepository
+    {
+        Task<List<SixSigmaIndicesViewModel>> GetSixSigmaIndicesAsync();
+        Task<SixSigmaIndicesViewModel?> GetSixSigmaIndicesByIdAsync(int id);
+        Task<OperationResult> InsertSixSigmaIndicesAsync(SixSigmaIndicesViewModel model);
+        Task<OperationResult> UpdateSixSigmaIndicesAsync(SixSigmaIndicesViewModel model);
+        Task<OperationResult> DeleteSixSigmaIndicesAsync(int id);
+    }
+}
--- QMS/Controllers/SEEIndicesController.cs
+++ QMS/Controllers/SEEIndicesController.cs
@@ -1,17 +1,100 @@
 ﻿using Microsoft.AspNetCore.Mvc;
-
+using QMS.Core.Models;
+using QMS.Core.Repositories.SixSigmaIndicesRepo;
 namespace QMS.Controllers
 {
-    public class SEEIndicesController: Controller
+    public class SEEIndicesController : Controller
     {
-        public IActionResult SEEIndicees()
+        private readonly ISixSigmaIndicesRepository _sixSigmaIndicesRepository;
+
+        public SEEIndicesController(ISixSigmaIndicesRepository sixSigmaIndicesRepository)
+        {
+            _sixSigmaIndicesRepository = sixSigmaIndicesRepository;
+        }
+
+        // LOAD VIEW
+        public IActionResult SEEIndices()
         {
             return View();
         }
-        public IActionResult SEEIndiceesdetail()
+
+        public async Task<IActionResult> SEEIndicesDetails(int Id)
         {
-            return View(); 
+            var model = new SixSigmaIndicesViewModel();
+
+            if (Id > 0)
+            {
+              
+                model = await _sixSigmaIndicesRepository.GetSixSigmaIndicesByIdAsync(Id);
+            }
+            else
+            {
+          
+                model.CreatedDate = DateTime.Now;
+            }
+
+            return View(model);
         }
 
+
+        // LIST ALL
+        [HttpGet]
+        public async Task<ActionResult> GetSEEIndicesListAsync()
+        {
+            var result = await _sixSigmaIndicesRepository.GetSixSigmaIndicesAsync();
+            return Json(result);
+        }
+
+        // DETAILS for EDIT
+        [HttpGet]
+        public async Task<ActionResult> GetSEEIndicesDetailsByIdAsync(int id)
+        {
+            var result = await _sixSigmaIndicesRepository.GetSixSigmaIndicesByIdAsync(id);
+            return Json(result);
+        }
+
+        // CREATE & UPDATE
+        [HttpPost]
+        public async Task<ActionResult> InsertUpdateSEEIndicesAsync(SixSigmaIndicesViewModel model)
+        {
+            if (!ModelState.IsValid)
+            {
+                var errors = ModelState.Values
+                                       .SelectMany(v => v.Errors)
+                                       .Select(e => e.ErrorMessage)
+                                       .ToList();
+
+                return Json(new { Success = false, Errors = errors });
+            }
+
+            var userId = HttpContext.Session.GetInt32("UserId");
+
+            if (model.Id > 0)
+            {
+                // UPDATE
+                model.UpdatedBy = userId?.ToString();
+                model.UpdatedDate = DateTime.Now;
+
+                var result = await _sixSigmaIndicesRepository.UpdateSixSigmaIndicesAsync(model);
+                return Json(result);
+            }
+            else
+            {
+                // INSERT
+                model.CreatedBy = userId?.ToString();
+                model.CreatedDate = DateTime.Now;
+
+                var result = await _sixSigmaIndicesRepository.InsertSixSigmaIndicesAsync(model);
+                return Json(result);
+            }
+        }
+
+        // DELETE
+        [HttpPost]
+        public async Task<ActionResult> DeleteSEEIndicesAsync(int id)
+        {
+            var result = await _sixSigmaIndicesRepository.DeleteSixSigmaIndicesAsync(id);
+            return Json(result);
+        }
     }
 }

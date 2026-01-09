3f7a8ed749d4830fef8fc2aa59043f21f3c6c8d2
rjrajput2811 <Bigboss2811@>
2026-01-09 15:58:23 +0530

    Deviation Note Design

---
+++ QMS/Controllers/DeviationNoteTracker.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace QMS.Controllers
+{
+    public class DeviationNoteTracker : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
